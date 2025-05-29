8d7921f4efbc8c9d1d2ec27a29ec1fe64d8ef032
KeshavxGupta <keshav40353@gmail.com>
2025-05-29 14:29:58 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
